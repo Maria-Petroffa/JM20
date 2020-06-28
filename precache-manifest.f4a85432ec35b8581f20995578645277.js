1ae8380071c57248f103de80f0599ee4e13b75ae
Maria Petrova <Maria Petrova>
2020-06-28 16:44:56 +0300

    Updates

---
+++ precache-manifest.f4a85432ec35b8581f20995578645277.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69487b06a62bf5c339466898cbfb3375",
+    "url": "/JM20/index.html"
+  },
+  {
+    "revision": "5d3d387c5f539497f326",
+    "url": "/JM20/static/css/2.117ce29f.chunk.css"
+  },
+  {
+    "revision": "845fe20d57fcb1871f07",
+    "url": "/JM20/static/css/main.a1096a80.chunk.css"
+  },
+  {
+    "revision": "5d3d387c5f539497f326",
+    "url": "/JM20/static/js/2.ff517f20.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/JM20/static/js/2.ff517f20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "845fe20d57fcb1871f07",
+    "url": "/JM20/static/js/main.79d260cb.chunk.js"
+  },
+  {
+    "revision": "fe305629335ab4953707",
+    "url": "/JM20/static/js/runtime-main.51ca69f5.js"
+  },
+  {
+    "revision": "7ce00256097ccf0d2c0641fce8a96e57",
+    "url": "/JM20/static/media/OpenSans-Regular.7ce00256.eot"
+  },
+  {
+    "revision": "a88748791c366be26c9bdf959d583d3f",
+    "url": "/JM20/static/media/OpenSans-Regular.a8874879.ttf"
+  },
+  {
+    "revision": "ce3b96ab2b51255e60d2d1aae633dae3",
+    "url": "/JM20/static/media/OpenSans-Regular.ce3b96ab.woff"
+  },
+  {
+    "revision": "0cb4787114f012d2daa4645df1811808",
+    "url": "/JM20/static/media/OpenSans-SemiBold.0cb47871.woff"
+  },
+  {
+    "revision": "5076adfa1bf9c1e5bac965ace755e798",
+    "url": "/JM20/static/media/OpenSans-SemiBold.5076adfa.ttf"
+  },
+  {
+    "revision": "e4dd295071829c9022725f034943525a",
+    "url": "/JM20/static/media/OpenSans-SemiBold.e4dd2950.eot"
+  }
+]);
